739112343cb5d307dd1131f02bcb7b874c7056a5
root <root@WIN-H3N8GBJO24D.localdomain>
2022-06-07 12:26:18 +0100

    ask complete

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,3 +4,27 @@
 
 def island_perimeter(grid):
     '''returns the perimeter of the island described in grid'''
+    counter = 0
+    grid_max = len(grid)
+    lst_max = len(grid[0])
+
+    for lst_idx, lst in enumerate(grid):
+        for land_idx, land in enumerate(lst):
+            if land == 1:
+                # left side
+                if lst[land_idx - 1] == 0:
+                    counter += 1
+
+                # top side
+                if grid[lst_idx - 1][land_idx] == 0:
+                    counter += 1
+
+                # right side
+                if lst[land_idx - 1] == 0:
+                    counter += 1
+
+                # bottom side
+                if grid[lst_idx + 1][land_idx] == 0:
+                    counter += 1
+
+    return counter
